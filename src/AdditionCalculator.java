57ccc6149b733aff7ee20d3bf6c37d6be0791faf
BZerachBerman <116837011+BZerachBerman@users.noreply.github.com>
2026-04-26 17:26:17 -0400

    Master now gets its problems from two clients and does basic processing to decide which calculator to send it to.

--- src/AdditionCalculator.java
+++ src/AdditionCalculator.java
@@ -1,3 +1,4 @@
+import java.util.concurrent.TimeUnit;
 public class AdditionCalculator {
     public int solve(mathProblem problem) {
         if (problem.operation().equals("addition")) {
@@ -7,10 +8,23 @@ public class AdditionCalculator {
     }
 
     private int add(int i, int i1) {
+
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+        System.out.println("addition calculator just spent 2 seconds adding " + i + " and " + i1);
         return i + i1;
     }
 
     private int subtract(int i, int i1) {
+        try {
+            TimeUnit.SECONDS.sleep(10);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+        System.out.println("addition calculator just spent 10 seconds subtracting " + i1 + " from " + i);
         return i - i1;
     }
 
