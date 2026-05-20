621888b70a06726a1ad29b4daad8efd9bd49e1c7
Aharon Gross <agross26@student.touro.edu>
2026-05-19 22:55:01 -0400

    Implement SlaveWriter queue socket send

--- src/Server/SlaveWriter.java
+++ src/Server/SlaveWriter.java
@@ -2,21 +2,38 @@ package Server;
 
 import Shared.mathProblem;
 
-import java.io.*;
+import java.io.PrintWriter;
 import java.util.concurrent.LinkedBlockingQueue;
 
 public class SlaveWriter extends Thread {
     private PrintWriter pw;
+    private LinkedBlockingQueue<mathProblem> problems;
 
     public SlaveWriter(PrintWriter pw, LinkedBlockingQueue<mathProblem> problems) {
         this.pw = pw;
+        this.problems = problems;
     }
 
-    // the point of the thread is to write to client
+    // Sends mathProblems from the queue to the slave over the socket
     public void run() {
+        System.out.println("SlaveWriter is running");
 
-        pw.println("Connected to Master.Master");
+        while (true) {
+            try {
+                mathProblem problem = problems.take();
 
+                String message = problem.problemID()
+                        + "," + problem.left()
+                        + "," + problem.operator()
+                        + "," + problem.right();
+
+                System.out.println("Master sending problem to slave: " + message);
+                pw.println(message);
+            }
+            catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                break;
+            }
+        }
     }
-
 }
